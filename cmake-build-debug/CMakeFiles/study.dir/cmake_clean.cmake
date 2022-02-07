082df07c3e6716784e1ed6a1568519a9a4b067e8
Dashuang <lidashuang@turingvideo.net>
2022-02-07 14:40:38 +0800

    init commit

---
+++ cmake-build-debug/CMakeFiles/study.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/study.dir/main.c.o"
+  "study"
+  "study.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/study.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,9 @@
+//
+// Created by 李大双 on 2022/2/7.
+//
+#include <stdio.h>
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
